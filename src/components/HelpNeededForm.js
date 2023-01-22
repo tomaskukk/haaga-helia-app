cf10de4cf6ace4262940ed2c7e7ca2040949ff7b
tomaskukk <tomsu_kukk@hotmail.com>
2023-01-22 12:36:38 +0200

    add help needed text form

---
+++ src/components/HelpNeededForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HelpNeededForm = () => (
+  <div 
+    style={{
+      margin: '0.25rem 0.5rem',
+      padding: '0 0.5rem',
+      borderRadius: '0.25rem'
+    }}
+  >
+    <h4>Want to help keep this page up to date?</h4>
+    <p>You can learn important skills of web development & get your name to this website (with ~200k views) by helping me keeping this up to date & developing it further, which will help you in job findings 😇. Contact me through the <a
+    href="https://docs.google.com/forms/d/e/1FAIpQLSe9UuRToGgMabKOWOGi7P83kwtG8QdlR9A59t_5BrgGXCQH1A/viewform?usp=sf_link"
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    feedback form
+  </a> if you're interested.</p>
+  </div>
+)
+
+export default HelpNeededForm
